a19c7bb32c07683209ca15f2e2e84ec0bd11c0be
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-22 16:46:58 +0100

    added tdp packets handling

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import constants
 import threading
+from tdp_protocol.parser import parser, tdp_packet
 
 
 # CZY NIE POWINNO BYĆ JEDNAK TAK, ŻE SERWER PRZYPISUJE PO WĄTKU KAŻDEMU ADRESOWI ("POŁĄCZENIU")?
@@ -19,19 +20,26 @@ class tdp_server:
     def listen(self):
         while True:
             data, address = self.sock.recvfrom(constants.buffer_size)
-            # TUTAJ DODAĆ przeparsowanie ze strumienia bajtów na listę pakietów
-
-            # Tutaj bez implementacji wielowątkowości, to się będzie działo w klasie-dziecku,
-            # jeśli ona sobie nie zaimplementuje podziału na wątki, to będzie mieć problem,
-            # gdy ktoś będzie złośliwie wysyłać bardzo dużo danych
-            self.handle_incoming(data.decode(constants.string_encoding), address)
+            # CZY TO JEST DOBRY SPOSÓB NA ZABEZPIECZENIE SIĘ PRZED NIEKOMPLETNYMI PAKIETAMI?
+            # CO JEŚLI KLIENT ZACZNIE DZIWNIE WYSYŁAĆ?
+            if data.__len__() % constants.packet_length != 0:
+                continue
+            bytes_array = bytearray(data)
+            while bytes_array.__len__() > 0:
+                packet_bytes = bytes_array[:constants.packet_length]
+                del bytes_array[:constants.packet_length]
+                packet = parser.bytes_to_packet(packet_bytes)
+                # Tutaj bez implementacji wielowątkowości, to się będzie działo w klasie-dziecku,
+                # jeśli ona sobie nie zaimplementuje podziału na wątki, to będzie mieć problem,
+                # gdy ktoś będzie złośliwie wysyłać bardzo dużo danych
+                self.handle_incoming(packet, address)
 
     def send(self):
         while True:
             # This is expected to be blocking
             not_parsed_data, address = self.handle_outgoing()
-            # TUTAJ DODAĆ parsowanie otrzymanych danych
-            bytes = not_parsed_data.encode(constants.string_encoding)
+            # TUTAJ DODAĆ walidację?
+            bytes = parser.packet_to_bytes(not_parsed_data)
             while bytes.__len__() > 0:
                 bytes_sent = self.sock.sendto(bytes, address)
                 bytes = bytes[bytes_sent:]
@@ -55,8 +63,12 @@ class tetris_server(tdp_server):
     def handle_outgoing(self):
         while True:
             # TUTAJ BĘDZIE czekanie aż coś się pojawi w buforze zamiast user inputu, to tylko dla sprawdzenia
-            not_parsed_data = input('Your message: ')
-            return not_parsed_data, ('127.0.0.1', 20000)
+            type_input = input('Packet type: ')
+            user_id_input = int(input('user_id: '))
+            opponent_id_input = int(input('opponent_id: '))
+            data_input = int(input('data: '))
+            packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
+            return packet, ('127.0.0.1', 20000)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
