5d1b53f78c5865890084fc5d6b053b4de596fe25
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-22 21:56:23 +0100

    removed redundant loop

--- server.py
+++ server.py
@@ -12,14 +12,13 @@ class tetris_server(tdp_socket_handler):
         print(f'Received from {address}: {parsed_data}')
 
     def handle_outgoing(self):
-        while True:
-            # TUTAJ BĘDZIE czekanie aż coś się pojawi w buforze zamiast user inputu, to tylko dla sprawdzenia
-            type_input = input('Packet type: ')
-            user_id_input = int(input('user_id: '))
-            opponent_id_input = int(input('opponent_id: '))
-            data_input = int(input('data: '))
-            packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
-            return packet, ('127.0.0.1', 20000)
+        # TUTAJ BĘDZIE czekanie aż coś się pojawi w buforze zamiast user inputu, to tylko dla sprawdzenia
+        type_input = input('Packet type: ')
+        user_id_input = int(input('user_id: '))
+        opponent_id_input = int(input('opponent_id: '))
+        data_input = int(input('data: '))
+        packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
+        return packet, ('127.0.0.1', 30000)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
