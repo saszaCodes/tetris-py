0c0efff7ae8d5b429d6ae556e9f7dbffd81425c0
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-22 18:27:42 +0100

    added descriptive comments

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
 import constants
 import threading
-from tdp_protocol.parser import parser, tdp_packet
+from tdp_protocol.parser import tdp_parser, tdp_packet
 
 
 # CZY NIE POWINNO BYĆ JEDNAK TAK, ŻE SERWER PRZYPISUJE PO WĄTKU KAŻDEMU ADRESOWI ("POŁĄCZENIU")?
+# Parent class, no objects should be created directly from it. Instead, create a child class
+# and implement handle_incoming and handle_outgoing methods.
+# The class runs two threads.
+# On one of them, it listens for incoming bytes, parses them udp packets and passes them to handle_incoming method.
+# On the other one, it waits until handle_outgoing (which is expected to be blocking) returns data and address,
+# parses the data (expected type: tdp_packet) into bytes and sends them to a returned address (expected type: tuple)
 class tdp_server:
     def __init__(self, host, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,7 +34,7 @@ class tdp_server:
             while bytes_array.__len__() > 0:
                 packet_bytes = bytes_array[:constants.packet_length]
                 del bytes_array[:constants.packet_length]
-                packet = parser.bytes_to_packet(packet_bytes)
+                packet = tdp_parser.bytes_to_packet(packet_bytes)
                 # Tutaj bez implementacji wielowątkowości, to się będzie działo w klasie-dziecku,
                 # jeśli ona sobie nie zaimplementuje podziału na wątki, to będzie mieć problem,
                 # gdy ktoś będzie złośliwie wysyłać bardzo dużo danych
@@ -39,7 +45,7 @@ class tdp_server:
             # This is expected to be blocking
             not_parsed_data, address = self.handle_outgoing()
             # TUTAJ DODAĆ walidację?
-            bytes = parser.packet_to_bytes(not_parsed_data)
+            bytes = tdp_parser.packet_to_bytes(not_parsed_data)
             while bytes.__len__() > 0:
                 bytes_sent = self.sock.sendto(bytes, address)
                 bytes = bytes[bytes_sent:]
--- tdp_protocol/parser.py
+++ tdp_protocol/parser.py
@@ -2,6 +2,9 @@ import constants
 # TUTAJ DODAĆ (tzn. w całym pliku) komentarze, żeby było wiadomo co się dzieje
 
 # TUTAJ DODAĆ jeszcze sprawdzenie, że player_id i opponent_id nie są takie same
+# Class creating a tdp packet. Its main purpose is to validate data in the packet to avoid bugs.
+# It doesn't let one create a new packet unless passed arguments are
+# string, int, int and a list of lists containing Cell_State objects respectively
 class tdp_packet:
     def __init__(self, type, player_id, opponent_id, data):
         self._type = None
@@ -72,8 +75,8 @@ class tdp_packet:
     def data(self, value):
         self._data = value
 
-
-class parser:
+# Class parsing bytes into tdp_packet objects and vice versa
+class tdp_parser:
     # MOŻE DAŁOBY SIĘ ZROBIĆ PODZIAŁ NA PARSER SERWEROWY I KLIENCKI, ŻEBY SERWEROWY NIE MUSIAŁ
     # ODTWARZAĆ CAŁEJ WIADOMOŚCI?
 
@@ -100,12 +103,17 @@ class parser:
 
     @staticmethod
     def bytes_to_packet(provided_bytes):
+        # ## Validate passed argument. If it is not of type bytearray or it is a bytearray but
+        # ## its length doesn't match expected tdp packet length, throw an error
         if not isinstance(provided_bytes, bytearray):
             raise TypeError('provided_bytes has to be a bytearray object')
+            # CZY TU POWINNO BYĆ RETURN?
         elif provided_bytes.__len__() != constants.packet_length:
             raise ValueError(f'provided_bytes length is incorrect; it should be {constants.packet_length}')
+            # CZY TU POWINNO BYĆ RETURN?
         else:
             # TUTAJ POPRAWIĆ sprawdzanie (w constants powinien być two-way dictionary albo coś)
+            # ## Split received bytearray into individual variables corresponding to the fields of a tdp packet
             code_bytes = provided_bytes[:constants.header_length]
             del provided_bytes[:constants.header_length]
             del provided_bytes[:constants.buffer_length]
@@ -115,18 +123,19 @@ class parser:
             del provided_bytes[:constants.opponent_id_length]
             data_bytes = provided_bytes[:]
             del provided_bytes[:]
-            # Parse packet code
+            # ## Parse packet code
             code_int = int.from_bytes(code_bytes, constants.byteorder)
             # CZY TU POWINNAM WYWALAĆ ERROR JEŚLI NIE MA TEGO W ZDEFINIOWANYCH TYPACH? PEWNIE TAK
             for const in constants.packet_types.__dict__:
                 if constants.packet_types.__dict__[const].code == code_int:
                     type_str = constants.packet_types.__dict__[const].name
-            # Parse player_id and opponent_id
+            # ## Parse player_id and opponent_id
             player_id_int = int.from_bytes(player_id_bytes, constants.byteorder)
             opponent_id_int = int.from_bytes(opponent_id_bytes, constants.byteorder)
-            # Parse data
+            # ## Parse data
             # TUTAJ DODAĆ parsowanie danych w odpowiedni sposób, teraz placeholder
             data_int = int.from_bytes(data_bytes, constants.byteorder)
+            # ## Create a new tdp_packet using parsed data
             return tdp_packet(type_str, player_id_int, opponent_id_int, data_int)
 
     # CZY PAKIET W OGÓLE POWINIEN ZAWIERAĆ OPPONENT ID?
@@ -134,10 +143,13 @@ class parser:
     @staticmethod
     def packet_to_bytes(packet):
         # CZY POTRZEBA SPRAWDZAĆ, ŻE PAKIET MA WSZYSTKIE ATRYBUTY? CHĘTNIE BYM ZAŁOŻYŁA, ŻE SKORO KLASA WYMAGA PODANIA ARGUMENTÓW DLA WSZYSTKICH ATRYBUTÓW PRZY TWORZENIU OBIEKTU, TO WYSTARCZY
+        # ## Validate passed argument. If its type is a tdp_packet object, throw an error
         if not isinstance(packet, tdp_packet):
             error_message = 'Incorrect data type of packet. Use provided tdp_packet type.'
             raise TypeError(error_message)
             # CZY TO POWINNAM DAĆ RETURN?
+        # ## If packet's type is 'play', look up its type's code and parse it to bytes.
+        # ## Fill other fields of the packet with 0s (they are irrelevant for this type)
         elif packet.type == constants.packet_types.play.name:
             code_int = constants.packet_types.play.code
             code_bytes = int.to_bytes(code_int, constants.header_length, constants.byteorder)
@@ -148,6 +160,8 @@ class parser:
                                        constants.byteorder)
             packet = code_bytes + buffer_bytes + other_bytes
             return packet
+        # ## If packet's type is 'now_playing', look up its type's code and parse it to bytes.
+        # ## Parse player_id and opponent_id to bytes. Fill other fields with 0s
         elif packet.type == constants.packet_types.now_playing.name:
             code_int = constants.packet_types.now_playing.code
             code_bytes = int.to_bytes(code_int, constants.header_length, constants.byteorder)
@@ -159,8 +173,11 @@ class parser:
             data_bytes = int.to_bytes(0, constants.data_length, constants.byteorder)
             packet = code_bytes + buffer_bytes + player_id_bytes + opponent_id_bytes + data_bytes
             return packet
+        # TUTAJ DODAĆ parsowanie game data
         elif packet.type == constants.packet_types.game_data.name:
             return
+        # ## If packet's type is 'end_game', look up its type's code and parse it to bytes.
+        # ## Parse player_id and opponent_id to bytes. Fill other fields with 0s
         elif packet.type == constants.packet_types.end_game.name:
             code_int = constants.packet_types.end_game.code
             code_bytes = int.to_bytes(code_int, constants.header_length, constants.byteorder)
