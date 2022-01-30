ad7835d91ed8fa643ca26d44b1bb97707f5280ea
tepeszCodes <tomek.szymanderski@gmail.com>
2022-01-30 14:31:07 +0100

    updated imports

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
+import random
+from constants import constants
 from tdp_protocol.socket_handler import tdp_socket_handler
-from tdp_protocol.parser import tdp_packet
+from tdp_protocol.parser.common import tdp_packet
 
 
 # print(constants.packet_types)
@@ -13,6 +15,16 @@ from tdp_protocol.parser import tdp_packet
 #     bytes = message.encode(constants.string_encoding)
 #     sock.sendto(bytes, ('127.0.0.1', 20000))
 
+def print_game_state_pretty(state):
+    for (index, row) in enumerate(state):
+        row_s = ""
+        separator = "_____________________________________________________________"
+        for column in row:
+            row_s += f'{column}\t'
+        print(row_s)
+        if index == state.__len__() - 1:
+            print(separator)
+
 class tetris_client(tdp_socket_handler):
     def __init__(self, host, port):
         super().__init__(host, port)
@@ -25,12 +37,20 @@ class tetris_client(tdp_socket_handler):
         type_input = input('Packet type: ')
         user_id_input = int(input('user_id: '))
         opponent_id_input = int(input('opponent_id: '))
-        data_input = int(input('data: '))
+        game_state = []
+        for i in range(constants.no_of_rows):
+            row = []
+            for j in range(constants.no_of_columns):
+                row.append(random.randint(0, 15))
+            game_state.append(row)
+        print('Sending following game data to the server:')
+        print_game_state_pretty(game_state)
+        data_input = game_state
         packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
         return packet, ('127.0.0.1', 20000)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# client = tetris_client('127.0.0.1', 30000)
-# client.start_client()
+client = tetris_client('127.0.0.1', 30000)
+client.start_client()
--- server.py
+++ server.py
@@ -1,6 +1,17 @@
+import time
 from tdp_protocol.socket_handler import tdp_socket_handler
-from tdp_protocol.parser import tdp_packet
+from tdp_protocol.parser.common import tdp_packet
 
+
+def print_game_state_pretty(state):
+    for (index, row) in enumerate(state):
+        row_s = ""
+        separator = "_____________________________________________________________"
+        for column in row:
+            row_s += f'{column}\t'
+        print(row_s)
+        if index == state.__len__() - 1:
+            print(separator)
 
 class tetris_server(tdp_socket_handler):
     def __init__(self, host, port):
@@ -10,15 +21,18 @@ class tetris_server(tdp_socket_handler):
     # CZY TO JEST PROBLEM, ŻE O TYM NIE WSPOMINAM W NADKLASIE (ZOBACZ UWAGĘ JAKĄ DAJE EDYTOR DO NAZWY KLASY)?
     def handle_incoming(self, parsed_data, address):
         print(f'Received from {address}: {parsed_data}')
+        print('Received game state:')
+        print_game_state_pretty(parsed_data.data)
 
     def handle_outgoing(self):
         # TUTAJ BĘDZIE czekanie aż coś się pojawi w buforze zamiast user inputu, to tylko dla sprawdzenia
-        type_input = input('Packet type: ')
-        user_id_input = int(input('user_id: '))
-        opponent_id_input = int(input('opponent_id: '))
-        data_input = int(input('data: '))
-        packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
-        return packet, ('127.0.0.1', 30000)
+        # type_input = input('Packet type: ')
+        # user_id_input = int(input('user_id: '))
+        # opponent_id_input = int(input('opponent_id: '))
+        # data_input = int(input('data: '))
+        # packet = tdp_packet(type_input, user_id_input, opponent_id_input, data_input)
+        # return packet, ('127.0.0.1', 30000)
+        time.sleep(1000)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
